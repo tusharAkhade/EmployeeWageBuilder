2e488423abe09838718f127441edd2d1f9d2c0cd
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-14 21:29:44 +0530

    Refactor: using OOPs concept

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,14 +2,18 @@ public class EmployeeWage
 {
 	public static void main(String args[])
 	{
-		int is_fulltime_present = 1;
-		int is_parttime_present = 2;
-		int wage_per_hr = 20;
+		Demo d = new Demo();
+		d.employeeWage();	
+	}
+	void employeeWage() {
+		final int max_working_hrs = 100;
+		final int days_in_month=20;
+		final int wage_per_hr = 20;
 		int working_hr = 0;
 		int total_working_hr = 0;
-		int days_in_month=20;
-		int max_working_hrs = 100;
 		int monthly_total_wage = 0;
+		int is_fulltime_present = 1;
+		int is_parttime_present = 2;
 		int daily_wage_array[] = new int[20];
 		for (int i=0; i<days_in_month && total_working_hr<max_working_hrs; i++) {
 			int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
@@ -33,4 +37,5 @@ public class EmployeeWage
 		}
 		System.out.println("\nMonthly wage is " + monthly_total_wage);
 	}
+
 }
